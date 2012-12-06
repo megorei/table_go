45d67e5433d08fc964d00c1c8dae0490a4bc5b38
exsemt <exsemt@gmail.com>
2012-12-06 11:38:50 +0100

    csv html safe

--- lib/table_go/helpers.rb
+++ lib/table_go/helpers.rb
@@ -3,7 +3,7 @@ module TableGo
 
     def table_go_for(collection, model_klass, options = {}, &block)
       if request && request.format.csv?
-        capture { TableGo.render_csv(collection, model_klass, options, &block) }
+        capture { TableGo.render_csv(collection, model_klass, options, &block).html_safe }
       else
         capture { TableGo.render_html(collection, model_klass, self, options, &block) }
       end
